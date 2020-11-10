e9c475bb1f7929d2b51a0f24cab6c33f93f69166
Rajat Malik <er.rajatmalik@gmail.com>
2020-11-10 15:03:23 +0530

    update even  odd position code linked lidt

--- Linked-list/odd-event-position-detection-linked-list.js
+++ Linked-list/odd-event-position-detection-linked-list.js
@@ -1,45 +1,32 @@
 function LinkedList() {
 	this.head = null;
-	this.tail = null;
 }
-function Node(value, next, prev) {
+function Node(value, next) {
 	this.value = value;
 	this.next = next;
-	this.prev = prev;
 }
 LinkedList.prototype.addToHeader = function (value) {
 	let newNode = new Node(value, this.head, null);
-	if (this.head) {
-		this.head.prev = newNode;
-	} else {
-		this.tail = newNode;
-	}
 	this.head = newNode;
 };
 
 let a = new LinkedList();
-a.addToHeader(23);
-a.addToHeader(24);
-a.addToHeader(25);
-a.addToHeader(26);
-a.addToHeader(27);
-a.addToHeader(28);
-a.addToHeader(29);
-a.addToHeader(30);
+a.addToHeader(2);
+a.addToHeader(4);
+a.addToHeader(5);
 
-const detectOddEvenLinkedList = () => {
-	let o = a.head;
-	let e = a.head.next;
-	e.prev = null;
+const detectOddEvenLinkedList = (list) => {
+	if (list === null) return null;
+	let o = list;
+	let e = list.next;
+	let eHead = e;
 	while (e && e.next) {
 		o.next = e.next;
-		e.next ? (e.next.prev = o) : null;
 		o = o.next;
-		e.next = o ? o.next : null;
-		o.next.prev = e;
+		e.next = o.next;
 		e = e.next;
 	}
-	console.log(o, e);
+	o.next = eHead;
+	return list;
 };
-
-detectOddEvenLinkedList();
+console.log(detectOddEvenLinkedList(a.head));
