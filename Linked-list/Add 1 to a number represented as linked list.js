96eefb01cc8efbdcef9e26476ba354ca0f4334a0
Rajat Malik <er.rajatmalik@gmail.com>
2020-11-22 12:27:51 +0530

    update filter

--- Linked-list/Add 1 to a number represented as linked list.js
+++ Linked-list/Add 1 to a number represented as linked list.js
@@ -19,7 +19,9 @@ LinkedList.prototype.addToHeader = function (value) {
 
 let a = new LinkedList();
 
+a.addToHeader(1);
 a.addToHeader(9);
+// a.addToHeader(9);
 // a.addToHeader(9);
 // a.addToHeader(25);
 // a.addToHeader(26);
@@ -28,36 +30,31 @@ a.addToHeader(9);
 // a.addToHeader(29);
 // a.addToHeader(30);
 
-function AddOne(list) {
-	// if (list.next === null) return list;
+function AddOne(list1) {
+	let list = list1.head;
 	while (list.next !== null) {
 		list = list.next;
 	}
 	let passRemainder = 0;
 	while (list.prev !== null) {
 		let res = null;
-		if (passRemainder !== 0) {
-			res = list.value + passRemainder + 1;
-		} else {
-			res = list.value + 1;
-		}
+		res = list.value + passRemainder + 1;
 		list.value = res % 10;
-		passRemainder = res / 10;
+		passRemainder = Math.round(res / 10);
 		list = list.prev;
 	}
 	if (list.prev === null) {
 		let res;
 		res = list.value + passRemainder + 1;
 		list.value = res % 10;
-		passRemainder = res / 10;
+		passRemainder = Math.round(res / 10);
 		if (passRemainder !== 0) {
-			a.addToHeader(passRemainder);
+			list1.addToHeader(passRemainder);
 		}
 	}
-
-	return list;
+	return list1;
 }
 console.log(
 	"🚀 ~ file: Add 1 to a number represented as linked list.js ~ line 36 ~ AddOne(a.head)",
-	AddOne(a.head)
+	AddOne(a)
 );
