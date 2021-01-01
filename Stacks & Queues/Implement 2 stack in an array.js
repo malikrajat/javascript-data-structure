81b6a34a9dd6b3bb2f78bad68adeed04b8d81c1c
Rajat <er.rajatmalik@gmail.com>
2021-01-01 08:58:53 +0530

    update cd=ode

--- Stacks & Queues/Implement 2 stack in an array.js
+++ Stacks & Queues/Implement 2 stack in an array.js
@@ -1,13 +1,48 @@
 // Implement 2 stack in an array
-const stackIns = require("./Stack");
-const stack = new stackIns.Stack();
-stack.push(11);
-stack.push(12);
-stack.push(15);
-stack.push(19);
-stack.push(89);
-console.log(stack.peek());
-console.log(stack.size());
-stack.pop();
-console.log(stack.peek());
-console.log(stack.size());
+function Stack(n) {
+	this.a = [];
+	this.counter = n;
+	this.top1 = -1;
+	this.top2 = this.counter;
+}
+Stack.prototype.push1 = function (ele) {
+	if (this.top1 < this.top2 - 1) {
+		this.a[++this.top1] = ele;
+	} else {
+		return false;
+	}
+};
+Stack.prototype.push2 = function (ele) {
+	if (this.top1 < this.top2 - 1) {
+		this.a[--this.top2] = ele;
+	} else {
+		return false;
+	}
+};
+Stack.prototype.pop1 = function (ele) {
+	if (this.top1 >= 0) {
+		let elem = this.a[this.top1];
+		this.top1--;
+		return elem;
+	} else {
+		return false;
+	}
+};
+Stack.prototype.pop2 = function (ele) {
+	if (this.top2 < this.counter) {
+		let elm = this.a[this.top2];
+		this.top2++;
+		return elm;
+	} else {
+		return false;
+	}
+};
+
+let stack = new Stack();
+stack.push1(11);
+stack.push2(12);
+stack.push1(15);
+stack.push2(19);
+stack.push1(89);
+stack.pop2();
+stack.pop1();
